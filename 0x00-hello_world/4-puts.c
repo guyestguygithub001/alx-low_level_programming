0a2d336524798d91e3f4129b1aba4e580f0a2988
daniel ene <danielene050@gmail.com>
2023-07-13 16:18:20 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - this is the main function
- * 
+ * Description: main - prints out a string to stdout.
  * Return: always 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
