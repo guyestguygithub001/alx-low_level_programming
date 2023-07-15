b13a4c1e5de73cbbac62cb41b12acfcac453aef9
daniel ene <danielene050@gmail.com>
2023-07-15 05:48:05 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,15 +6,17 @@
  * Return: Always 0 (success)
  */
 
-void print_numbers() {
-	  int i;
+void print_numbers(void)
+{
+int i;
 
-	    for (i = 0; i < 10; i++) {
-		        printf("%d\n", i);
-			  }
+for (i = 0; i < 10; i++)
+{
+printf("%d\n", i);
 }
-
-int main(void) {
-	  print_numbers();
-	    return 0;
+}
+int main(void)
+{
+print_numbers();
+return 0;
 }
