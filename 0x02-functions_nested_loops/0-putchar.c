41c0f2ff6a4a8113f75dc80ad00df2eaca8ae4bc
daniel ene <danielene050@gmail.com>
2023-07-18 07:24:30 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print _putchar
+ * is written by ene
+ * Return: 0
+ */
+
+
+int main(void)
+{
+
+printf("_putchar\n");
+
+return (0);
+}
