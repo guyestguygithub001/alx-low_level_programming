e7822cd6852077b5d2db850344efe2a1df3c55e3
daniel ene <danielene050@gmail.com>
2023-08-17 21:40:35 -0700

    both tasks

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,23 +2,30 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+/**
+ * print_numbers - Prints the numbers, followed by a new line.
+ * @separator: pritns the string between numbers.
+ * @n: number of int passed to the function.
+ * @...: variable number of numbers to be printed.
+ */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list nums;
-    unsigned int index;
+	va_list nums;
+	unsigned int index;
 
-    va_start(nums, n);
+	va_start(nums, n);
 
-    for (index = 0; index < n; index++)
-    {
-        int num = va_arg(nums, int);
-        printf("%d", num);
+	for (index = 0; index < n; index++)
+	{
+		int num = va_arg(nums, int);
 
-        if (index != n - 1 && separator != NULL)
-            printf("%s", separator);
-    }
+		printf("%d", num);
+		if (index != n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
 
-    printf("\n");
+	printf("\n");
 
-    va_end(nums);
+	va_end(nums);
 }
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print-strings - String to be printed followed by a new line
+ * @separator: Print the strings between strings
+ * @n: number of strings passed into the function
+ * @...: A variable number of strings to be printed
+ *
+ * Description: If separator = NULL, it is not printed
+ *              If one of the strings = NULL, "(nil)" is printed
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+    va_list strings;
+    unsigned int index;
+    char *str;
+
+    va_start(strings, n);
+
+    for (index = 0; index < n; index++)
+    {
+        str = va_arg(strings, char *);
+
+        if (str == NULL)
+            printf("(nil)");
+        else
+            printf("%s", str);
+
+        if (index != n - 1 && separator != NULL)
+            printf("%s", separator);
+    }
+
+    printf("\n");
+
+    va_end(strings);
+}
