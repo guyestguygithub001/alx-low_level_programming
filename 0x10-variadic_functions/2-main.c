440cc576405789f5c9971e5e0c63a69c40a8256c
daniel ene <danielene050@gmail.com>
2023-08-17 21:52:16 -0700

    Dem say one woman constant na another kele variable

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -1,8 +1,12 @@
-#include <stdio.h>
 #include "variadic_functions.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    print_strings(", ", 3, "Hello", "World", "!");
-    return 0;
+	print_strings(", ", 2, "Jay", "Django");
+	return (0);
 }
