8a7eb22cc4fb43d91bdb373c145a4c05d1b15a82
daniel ene <danielene050@gmail.com>
2023-10-16 17:18:26 -0700

    TIme to go to sleep

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -2,34 +2,35 @@
 
 int add(int a, int b)
 {
-        return a + b;
+	return a + b;
 }
 
 int sub(int a, int b)
 {
-        return a - b;
+	return a - b;
+}
 
 int mul(int a, int b)
 {
-        return a * b;
+	return a * b;
 }
 
 int div(int a, int b)
 {
-        if (b == 0)
-        {
-                printf("Error: Division by zero\n");
-                return 0;
-        }
-        return a / b;
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		return 0;
+	}
+	return a / b;
 }
 
 int mod(int a, int b)
 {
-        if (b == 0)
-        {
-                printf("Error: Division by zero\n");
-                return 0;
-        }
-        return a %b;
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		return 0;
+	}
+	return a % b;
 }
