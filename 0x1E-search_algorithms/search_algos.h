287f42225d75f32b8f5eb58ac32dcca01d94b67b
guyestguygithub001 <danielene050@gmail.com>
2024-02-07 09:27:06 -0800

    1st fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,53 +1,53 @@
-#define SEARCH_ALGOS_H
-#ifndef SEARCH_ALGOS_H
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
 
 #include <stdlib.h>
-#include <math.h>
 #include <stdio.h>
 
 /**
- * struct listint_s - singly linked list,
+ * struct listint_s - singly linked list
  *
- * @n: Int,
- * @index: Index of node in list,
- * @next: Pointer to next node,
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
  *
- * Description: singly linked list, node structure,
- * for Holberton project,
+ * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
-		int n;
-		struct listint_s *next;
-		size_t index;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 /**
  * struct skiplist_s - Singly linked list with an express lane,
  *
- * @n: Int,
+ * @n: Integer,
  * @index: Index of node in list,
  * @next: Pointer to next node,
  * @express: Pointer to next node in express lane,
  *
+ * Description: singly linked list node structure with an express lane,
+ * for Holberton project,
  */
 typedef struct skiplist_s
 {
-
 	int n;
 	size_t index;
-	struct skiplist_s *express;
 	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
-int jump_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
-int advanced_binary(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 
-#endif /* SEARCH_ALGOS_H */
-
+#endif
